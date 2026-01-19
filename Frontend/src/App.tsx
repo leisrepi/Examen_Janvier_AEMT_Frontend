60c3cad92031f8af659ec93f50f4272ccc0fd670
leisrepi <la238369@student.helha.be>
2026-01-19 11:09:35 +0100

    folderComponent affiche les folder d'une manière statique (manque appel a l'api pour les sous dossier)

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import FolderComponent from './components/folderComponent'
+import type { Item } from './components/folderComponent'
+import type { Folder } from './types/Folder';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [foldersAndNotes, setFoldersAndNotes] = useState<Item[]>([
+    { idFolder: 1, nameFolder: "Work", idParent: 0 },
+    { idFolder: 2, nameFolder: "Personal", idParent: 0 }
+  ]);
 
+  const rootFolderName :string = "root";
+  
   return (
     <>
-      <FolderComponent/>
+      <FolderComponent rootFolderName={rootFolderName} foldersAndNotes={foldersAndNotes} />
     </>
   )
 }
--- Frontend/src/components/folderComponent.tsx
+++ Frontend/src/components/folderComponent.tsx
@@ -1,7 +1,30 @@
-export default function FolderComponent() {
+import type { Folder } from '../types/Folder';
+import type { Note } from '../types/Note';
+
+
+//melange des types pour faire fonctionner le map
+export type Item = Folder | Note;
+
+interface Props {
+    rootFolderName: string;
+    foldersAndNotes: Item[];
+}
+
+
+
+
+
+export default function FolderComponent({rootFolderName, foldersAndNotes}: Props)  {
   return (
-    <div>
-      <h1>Folder Component</h1>
+    <div className="FolderComponent">
+        <h2>{rootFolderName}</h2>
+        {foldersAndNotes.map((item : Item) => {
+                if ("nameFolder" in item) {
+                    let folder = item as Folder;
+                    return <FolderComponent rootFolderName={folder.nameFolder} foldersAndNotes={[]} />;
+                }
+            }
+        )}
     </div>
   )
 }
